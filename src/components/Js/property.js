d96286105fb0ea8f6fa208dbddc10092b88e1967
Gowtham AK <gowthamak812@gmail.com>
2022-06-21 19:29:11 +0530

    using basic animation in property page

--- src/components/Js/property.js
+++ src/components/Js/property.js
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import '../Sass/card.scss'
 import { AiOutlineHeart } from 'react-icons/ai';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Cards() {
   const [users, setUsers] = useState([])
@@ -27,6 +29,9 @@ export default function Cards() {
         console.log(error);
       });
   }
+  AOS.init({
+    duration: 1200,
+  })
   return (
     <>
       <Header />
@@ -35,13 +40,13 @@ export default function Cards() {
         <Row className='card-row'>
           {users && users.slice(0, 12).map((data, i) => {
             return (
-              <Col xs={12} md={6} lg={4} key={i} className="m-0">
+              <Col data-aos="slide-up" xs={12} md={6} lg={4} key={i} className="m-0">
                 <Link style={{ textDecoration: 'none' }} to={`/details?id=${i}`}>
                   <Card className='card-div'>
                     <AiOutlineHeart size={35} className='heart' />
                     <Card.Img className='card-image' src={data && data.Images[3].url} />
                     <Card.Body className='card-body'>
-                      <p>{data && data.Title}</p>
+                      <p>le donatello</p>
                       <p>{data && data.Bedrooms} bedroom apartment for {data && data.Property_Type}</p>
                       <p class="font-weight-bold">{data && data.Price} &euro; </p>
                     </Card.Body>

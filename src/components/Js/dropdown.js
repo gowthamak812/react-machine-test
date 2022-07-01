b656f448b5c90afbc3ccab50b06fc2871352c9e5
Gowtham AK <gowthamak812@gmail.com>
2022-07-01 16:26:35 +0530

    dropdown changes

--- src/components/Js/dropdown.js
+++ src/components/Js/dropdown.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import {Nav } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Dropdown() {
     return (
         <>
             <p className='normal-style'>Property for sale</p>
-            <Nav fill className='dropdown' variant="tabs">
+            <Nav fill className='dropdown' variant="tabs" border-none>
                 <Nav.Item xs-d-block>
                     <select title="All Bedrooms" id="basic-nav-dropdown">
+                        <option href="#">Property Type</option>
                         <option href="#">1 Bedroom</option>
                         <option href="#">2 Bedrooms</option>
                         <option href="#">3 Bedrooms</option>
@@ -18,6 +19,7 @@ function Dropdown() {
                 </Nav.Item>
                 <Nav.Item>
                     <select title="Any Neighbourhood" id="basic-nav-dropdown">
+                        <option href="#">Any Neighbourhood</option>
                         <option href="#">Larvatto</option>
                         <option href="#">Condamine</option>
                         <option href="#">Fontvieille</option>
@@ -27,24 +29,27 @@ function Dropdown() {
                 </Nav.Item>
                 <Nav.Item>
                     <select title="Min Price" id="basic-nav-dropdown">
-                        <option href="#">5000000 &#8364;</option>
+                        <option href="#">Min Price</option>
                         <option href="#">1000000 &#8364;</option>
+                        <option href="#">1500000 &#8364;</option>
                     </select>
                 </Nav.Item>
                 <Nav.Item>
                     <select xs={12} sm={12} md={12} title="Max Price" id="basic-nav-dropdown">
-                        <option href="#">2500000 &#8364;</option>
+                        <option href="#">Max Price</option>
                         <option href="#">2000000 &#8364;</option>
+                        <option href="#">3000000 &#8364;</option>
                     </select>
                 </Nav.Item>
                 <Nav.Item>
                     <select xs={12} sm={12} md={12} title="Sort by" id="basic-nav-dropdown">
+                        <option href="#">Sort by</option>
                         <option href="#">For Rental</option>
                         <option href="#">For Sale</option>
                     </select>
                 </Nav.Item>
                 <Nav.Item>
-                    <td>12 Results</td>
+                    <td className='result'>12 Results</td>
                 </Nav.Item>
             </Nav>
         </>

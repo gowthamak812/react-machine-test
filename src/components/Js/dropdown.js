0fa458cccf32e01e0ae2ad8499186b641b3991e5
Gowtham AK <gowthamak812@gmail.com>
2022-07-05 14:51:45 +0530

    dropdown make responsive

--- src/components/Js/dropdown.js
+++ src/components/Js/dropdown.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Nav } from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Dropdown() {
@@ -7,18 +7,17 @@ function Dropdown() {
         <>
             <p className='normal-style'>Property for sale</p>
             <Nav fill className='dropdown' variant="tabs" border-none>
-                <Nav.Item xs-d-block>
-                    <select title="All Bedrooms" id="basic-nav-dropdown">
+                <Nav.Item>
+                    <select className='select' title="All Bedrooms" id="basic-nav-dropdown">
                         <option href="#">Property Type</option>
                         <option href="#">1 Bedroom</option>
                         <option href="#">2 Bedrooms</option>
                         <option href="#">3 Bedrooms</option>
                         <option href="#">4 Bedrooms</option>
-
                     </select>
                 </Nav.Item>
                 <Nav.Item>
-                    <select title="Any Neighbourhood" id="basic-nav-dropdown">
+                    <select className='select' title="Any Neighbourhood" id="basic-nav-dropdown">
                         <option href="#">Any Neighbourhood</option>
                         <option href="#">Larvatto</option>
                         <option href="#">Condamine</option>
@@ -28,24 +27,27 @@ function Dropdown() {
                     </select>
                 </Nav.Item>
                 <Nav.Item>
-                    <select title="Min Price" id="basic-nav-dropdown">
-                        <option href="#">Min Price</option>
+                    <select className='select' title="Min Price" id="basic-nav-dropdown">
+                        <option href="#" value="a">Min Price</option>
                         <option href="#">1000000 &#8364;</option>
                         <option href="#">1500000 &#8364;</option>
-                    </select>
-                </Nav.Item>
-                <Nav.Item>
-                    <select xs={12} sm={12} md={12} title="Max Price" id="basic-nav-dropdown">
-                        <option href="#">Max Price</option>
                         <option href="#">2000000 &#8364;</option>
                         <option href="#">3000000 &#8364;</option>
                     </select>
                 </Nav.Item>
                 <Nav.Item>
-                    <select xs={12} sm={12} md={12} title="Sort by" id="basic-nav-dropdown">
+                    <select className='select' xs={12} sm={12} md={12} title="Max Price" id="basic-nav-dropdown">
+                        <option href="#">Max Price</option>
+                        <option href="#">1500000 &#8364;</option>
+                        <option href="#">2000000 &#8364;</option>
+                        <option href="#">3000000 &#8364;</option>
+                    </select>
+                </Nav.Item>
+                <Nav.Item>
+                    <select className='select' xs={12} sm={12} md={12} title="Sort by" id="basic-nav-dropdown">
                         <option href="#">Sort by</option>
-                        <option href="#">For Rental</option>
                         <option href="#">For Sale</option>
+                        <option href="#">For Rental</option>
                     </select>
                 </Nav.Item>
                 <Nav.Item>
--- src/components/Js/property.js
+++ src/components/Js/property.js
@@ -57,7 +57,6 @@ export default function Cards() {
                 <Col className='p-0' data-aos="slide-up" xs={12} md={6} sm={6} lg={4} key={i}>
                   <AiOutlineHeart onClick={() => ToggleSwitch(data.id)} size={40} className={fvtId == data.id && switchToggled == "heart-toggle" ? switchToggled : "heart"} />
                   <Link style={{ textDecoration: 'none' }} to={`/details?id=${i}`}>
-                    {/* <AiOutlineHeart href='#' onClick={ToggleSwitch} size={40} className={switchToggled ? "heart-toggle" : "heart"} /> */}
                     <Card className='card-div '>
                       <Image fluid className='card-image' src={data && data.Images[0].url} />
                       <Card.Body className='card-body'>
